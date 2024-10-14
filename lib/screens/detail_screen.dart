355c0d1fa6150f17aca0deb6fdc4ddf65a13ffe7
rezk1834 <moustafarezk@ejust.edu.eg>
2024-10-14 19:41:20 +0300

    Create the detail page with information about the selected item

---
+++ lib/screens/detail_screen.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+import '../models/item_model.dart';
+
+class DetailPage extends StatelessWidget {
+  final Item item;
+
+  DetailPage({required this.item});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        flexibleSpace: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Theme.of(context).primaryColor, Colors.blueAccent],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+        ),
+        title: Text(
+          "Item Details",
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
+        centerTitle: true,
+      ),
+      body: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [Colors.white, Colors.blue.shade50],
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+              ),
+            ),
+          ),
+          SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  // Title
+                  Text(
+                    item.title,
+                    style: TextStyle(
+                      fontSize: 28,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                  SizedBox(height: 16),
+                  // Image
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(15),
+                    child: Image.network(
+                      "https://static-00.iconduck.com/assets.00/flutter-icon-2048x2048-ufx4idi8.png",
+                      fit: BoxFit.cover,
+                      height: 200,
+                      width: double.infinity,
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  // Body Text
+                  Text(
+                    item.body,
+                    style: TextStyle(
+                      fontSize: 18,
+                      height: 1.5,
+                      color: Colors.black87,
+                    ),
+                    textAlign: TextAlign.justify,
+                  ),
+                  SizedBox(height: 80), // Space for button
+                ],
+              ),
+            ),
+          ),
+          Positioned(
+            bottom: 16,
+            left: 16,
+            right: 16,
+            child: ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Theme.of(context).primaryColor,
+                padding: EdgeInsets.symmetric(vertical: 10),
+                textStyle: TextStyle(fontSize: 20),
+                minimumSize: Size(double.infinity, 50),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+              child: Text("Go Back", style: TextStyle(color: Colors.white),),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
